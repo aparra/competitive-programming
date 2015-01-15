bdaa41432754a80c258324a2e2211c134d440856
eannpaa <anderson.parra.de.paula@ericsson.com>
2015-01-15 17:17:09 +0000

    solution for uva problem 01124 - Celebrity Jeopardy

---
+++ src/main/java/uva/problem/P01124_CelebrityJeopardy.java
@@ -0,0 +1,19 @@
+package uva.problem;
+
+import java.util.Scanner;
+
+/** 
+/* Problem: 1124 - Celebrity Jeopardy 
+/* Submission: 14773682
+/* Run Time: 0.199 s 
+ */
+public class P01124_CelebrityJeopardy {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			System.out.println(in.nextLine());
+		}
+		in.close();
+	}
+}
